82eb10c62c6fffe5c60abdc8ae659994a0a33a57
WonSeola <39373661+wonseola@users.noreply.github.com>
2024-04-23 01:49:38 +0900

    a

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,10 +1,10 @@
-import { URL } from "../app/(home)/page";
+import { URL_SRC } from "../app/(home)/page";
 import styles from "../styles/movie-videos.module.css";
 
 
 
 async function getVideos(id: string) {
-    const response = await fetch(`${URL}/${id}/videos`);
+    const response = await fetch(`${URL_SRC}/${id}/videos`);
     return response.json();
 }
 
