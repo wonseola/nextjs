a309672e6de61b5a3d6ab790c6f966fa8694eba3
WonSeola <39373661+wonseola@users.noreply.github.com>
2024-04-22 16:33:32 +0900

    next 개쩐다 진짜,,

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,46 @@
+
+
 export const metadata = {
     title: "Home",
 }
 
-export default function Page() {
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+
+async function getMovies() {
+    return fetch(URL).then(response => response.json());
+}
+
+export default async function HomePage() {
+    const movies = await getMovies();
     return (
         <div>
-            <h1>hello</h1>
+            {JSON.stringify(movies)}
         </div>
     )
 }
+
+// "use client";
+// import { useEffect, useState } from "react"
+
+
+
+// export default function Page() {
+//     const URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [movies, setMovies] = useState();
+//     const getMovies = async () => {
+//         const response = await fetch(URL);
+//         const json = await response.json();
+//         setMovies(json);
+//         setIsLoading(false);
+//     }
+//     useEffect(() => {
+//         getMovies();
+//     }, [])
+//     return (
+//         <div>
+//             {isLoading ? "Loading ... " : JSON.stringify(movies)}
+//         </div>
+//     )
+// }
